e9cd750c4d0d272f00568494d516019c922de270
mariasodiaz <mariasol.diaz@mercadolibre.com>
2022-09-23 12:49:58 -0300

    go bases I mañana

---
+++ goBasesI/ejercitacion.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//ej1
+	var nombre string
+	nombre = "Sol"
+
+	var direccion string
+	direccion = "M brin 222"
+
+	fmt.Printf("%v\n", nombre)
+	fmt.Printf("%v\n",direccion)
+
+
+	//ej2
+
+	var temperatura float64
+	temperatura = 18.3
+
+	var presion int
+	presion = 30
+
+	var humedad int
+	humedad = 40
+
+	fmt.Printf("%v\n",temperatura)
+	fmt.Printf("%v\n",presion)
+	fmt.Printf("%v\n",humedad)
+
+	//ej3
+	/*
+	var 1nombre string MAL
+  var apellido string bien
+  var int edad MAL
+  1apellido := 6 MAL
+  var licencia_de_conducir = true BIEN
+  var estatura de la persona int BIEN
+  cantidadDeHijos := 2 BIEN 
+
+	*/
+
+
+	//ej4
+
+	/*
+	var apellido string = "Gomez" BIEN
+  var edad int = "35" MAL
+  boolean := "false"; MAL
+  var sueldo string = 45857.90 MAL
+  var nombre string = "Julián" BIEN
+
+
+	*/
+}
+
---
+++ hola_mundo.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt" //-> para formato
+)
+
+const Segundos = 12
+
+
+
+func main(){
+	fmt.printf("Hola mundo")
+	fmt.printf("%d",Segundos)
+}
